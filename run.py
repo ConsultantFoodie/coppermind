5475fbf7ca6f82e883e10164be6f1f6f48906ae4
Hardik <hardikti@gmail.com>
2021-01-02 19:02:16 +0530

    Adds port for heroku

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ import os
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
