6d36d554b16f33c5400dea33400dbc595a6a74a8
Hardik <hardikti@gmail.com>
2021-01-03 20:20:18 +0530

    Adds heroku postgres db

--- coppermind/main.py
+++ coppermind/main.py
@@ -8,7 +8,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sazed.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
